504171a3d5c967162ebbe1dd876fd990abe77c08
Topanga <mdy@onpick.com>
2025-07-21 18:15:21 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 def main():
     name = input("Enter the name of the user: ")
-    print(f"Hello {name}")
+    if not name:
+        print("No name provided.")
+    else:
+        print(f"Hello {name}")
 
 if __name__ == "__main__":
     main()
